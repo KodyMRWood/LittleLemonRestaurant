7ff69630393a27d12f7a294924c81d6c60124837
Kody M.R. Wood <55820018+KodyMRWood@users.noreply.github.com>
2025-10-16 14:42:28 -0400

    Added Testing for form validation, added attributes to index.js to prevent warnings
    Added testing for the booking form to validate it works.Added test ids to the input to be able to find them efficiently in test.js. Attributes in for the BrowserRouter in the injdex.js is to stop warning about future use of the code and thus to future proof the work.

--- littlelemonrestaurant/src/Components/BookingComponents/Booking.test.js
+++ littlelemonrestaurant/src/Components/BookingComponents/Booking.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import BookingPage from './BookingPage';
+import BookingForm from './BookingForm';
 import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 
 const page= require('./BookingPage');
+const form = require('./BookingForm')
 
 test( 'Renders BookingForm', () => {
     render(<BrowserRouter><BookingPage></BookingPage></BrowserRouter>);
@@ -13,4 +16,52 @@ test( 'Renders BookingForm', () => {
     expect(headingElement).toBeInTheDocument(),
     expect(initTime.length).toBeGreaterThan(0);
     expect(updateTimesTest.length).toBeGreaterThan(0);
+});
+
+test( 'Form works properly', () => {
+
+    const availableTimes = ["17:00", "17:30"];
+    const setAvailableTimes = jest.fn();
+    render(<BookingForm availableTimes={availableTimes} setAvailableTimes={setAvailableTimes}></BookingForm>);
+    const dateInput = screen.getByTestId('res-date');
+    const timeInput = screen.getByTestId('res-time')
+    const guestInput = screen.getByTestId('guests')
+    const occasionInput = screen.getByTestId('occasion');
+
+    expect(dateInput).toBeInTheDocument(),
+    expect(dateInput).toHaveAttribute('required'),
+    expect(dateInput).toHaveAttribute('min', "2025-10-16"),
+    fireEvent.change(dateInput,{target:{value:"2025-10-16"}});
+    expect(dateInput.value).toEqual("2025-10-16"),
+    fireEvent.change(dateInput,{target:{value:"2025-10-10"}});
+    expect(dateInput.validity.rangeUnderflow).toBe(true);
+
+
+    expect(timeInput).toBeInTheDocument(),
+    expect(timeInput).toHaveAttribute('required'),
+    expect(timeInput).toHaveAttribute('min',"17:00"),
+    fireEvent.change(timeInput,{target:{value:"17:30"}}),
+    expect(timeInput.validity.valid).toBe(true),
+    expect(timeInput).toHaveValue("17:30"),
+    fireEvent.change(timeInput,{target:{value:"19:00"}}),
+    // This should be none since it is not in the available times above
+    // So it cant actually select the value;
+    expect(timeInput).toHaveValue("none"),
+    
+    expect(guestInput).toBeInTheDocument(),
+    expect(guestInput).toHaveAttribute('required'),
+    expect(guestInput).toHaveAttribute('min','1'),
+    expect(guestInput).toHaveAttribute('max', '10'),
+    fireEvent.change(guestInput,{target:{value:5}}),
+    expect(guestInput).toHaveValue(5),
+    expect(guestInput.validity.valid).toBe(true),
+    fireEvent.change(guestInput,{target:{value:0}}),
+    expect(guestInput.validity.rangeUnderflow).toBe(true),
+    fireEvent.change(guestInput,{target:{value:30}}),
+    expect(guestInput.validity.rangeOverflow).toBe(true),
+    
+    
+    expect(occasionInput).toBeInTheDocument(),
+    userEvent.selectOptions(occasionInput, 'birthday'),
+    expect(occasionInput).toHaveValue('birthday')
 });
--- littlelemonrestaurant/src/Components/BookingComponents/BookingForm.js
+++ littlelemonrestaurant/src/Components/BookingComponents/BookingForm.js
@@ -1,34 +1,61 @@
 import  { useState } from 'react'
 
+// Getting todays date and time
+const today = new Date()
+const year = today.getFullYear();
+const month = String(today.getMonth() + 1).padStart(2, '0'); // Months are 0-indexed
+const day = String(today.getDate()).padStart(2, '0');
+const todayFormatted = `${year}-${month}-${day}`;
+
+const currentTime = today.getHours()+ ":" + today.getMinutes();
+
 
 function BookingForm(props) {
-    const [date, setDate] = useState(new Date());
+    const [date, setDate] = useState(todayFormatted);
     const [time, setTime] = useState('none');
     const [guests, setGuests] = useState(1);
     const [occasion, setOccasion] = useState('none');
 
+
+    const [dateChanged, setDateChanged] = useState(false);
+    const [timeChanged, setTimeChanged] = useState(false);
+    const [guestsChanged, setGuestsChanged] = useState(false);
+
     function handleDateChange(e){
         setDate(e.target.value);
         props.setAvailableTimes(e.target.value);
+        setDateChanged(true);
+    }
+    
+    function handleTimeChange(e)
+    {
+        setTime(e.target.value);
+        setTimeChanged(true);
+    }
+
+    function handleGuestsChange(e)
+    {
+        setGuests(e.target.value);
+        setGuestsChanged(true);
     }
 
   return (
 
     <form onSubmit={(e) => e.preventDefault()} id="BookingForm">
         <label htmlFor="res-date">Choose date*</label>
-        <input type="date" id="res-date" name="res-date" value={date} onChange={handleDateChange} required/>
+        <input type="date" id="res-date" data-testid="res-date" name="res-date" value={date} onChange={handleDateChange} min={todayFormatted} required/>
 
         <label htmlFor="res-time">Choose time*</label>
-        <select id="res-time" name="res-time" value={time} onChange={(e)=> setTime(e.target.value)} required>
+        <select id="res-time" data-testid="res-time" name="res-time" value={time} min="17:00" onChange={handleTimeChange} required>
             <option value="none">Pick a Time</option>
-            {props.availableTimes.map((time) => <option key={time} value={time}>{time}</option>)}
+            {props.availableTimes.map((time) => (currentTime < time) ? <option key={time} value={time}>{time}</option> : false)}
         </select>
 
         <label htmlFor="guests">Number of guests*</label>
-        <input type="number" placeholder='1' min="1" max="10" id="guests" name="guests" value={guests} onChange={(e)=> setGuests(e.target.value)} required/>
+        <input type="number" placeholder='1' min="1" max="10" id="guests" data-testid="guests" name="guests" value={guests} onChange={handleGuestsChange} required/>
 
         <label htmlFor="occasion">Occasion</label>
-        <select id="occasion" name="occasion" value={occasion} onChange={(e)=> setOccasion(e.target.value)}>
+        <select id="occasion" data-testid="occasion" name="occasion" value={occasion} onChange={(e)=> setOccasion(e.target.value)}>
             <option value="none">None</option>
             <option value="birthday">Birthday</option>
             <option value="anniversary">Anniversary</option>
@@ -36,7 +63,7 @@ function BookingForm(props) {
             <option value="promotion">Promotion</option>
         </select>
 
-        <button type="submit" value="Confirm Reservation" onClick={(e) => {props.submit(e,new FormData(document.forms.BookingForm))}}>Confirm Reservation</button>
+        <button type="submit" value="Confirm Reservation" disabled={!(timeChanged)} onClick={(e) => {props.submit(e,new FormData(document.forms.BookingForm))}}>Confirm Reservation</button>
     </form>
   )
 }
